c3654f3d394171be3a5450fe9674a04bde0c9234
Emerson de Oliveira <emerson_yeda@hotmail.com>
2025-09-18 00:06:48 -0300

    Inicio do Pokedex

---
+++ Aulas-Boniek/LutaFuncional/assets/js/functions.js
@@ -0,0 +1,138 @@
+
+// Modelo para criação de personagem - inicio
+
+const defaultCharacter = {
+    name: '',
+    life: 1,
+    maxLife: 1,
+    attack: 0,
+    defense: 0
+}
+// Modelo para criação de personagem - fim
+
+// Personagem guerreiro - inicio
+
+const createKnight = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 100,
+        maxLife: 100,
+        attack: 10,
+        defense: 8
+    }
+}
+
+// Personagem guerreiro - fim
+
+// Personagem mago - inicio
+
+const createSorcerer = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 50,
+        maxLife: 50,
+        attack: 14,
+        defense: 3
+    }
+}
+
+// Personagem mago - fim
+
+// Personagem Mostro pequeno - inicio
+
+const createLittleMonster = () => {
+    return {
+        ...defaultCharacter,
+        name: 'Little Monster',
+        life: 40,
+        maxLife: 40,
+        attack: 4,
+        defense: 4
+    }
+}
+
+// Personagem Mostro pequeno - Fim
+
+// Personagem Grande - inicio
+
+const createBigMonster = () => {
+    return {
+        ...defaultCharacter,
+        name: 'Big Monster',
+        life: 120,
+        maxLife: 120,
+        attack: 16,
+        defense: 6
+    }
+}
+
+// Personagem Grande - Fim
+
+const stage = {
+    fighter1: null,
+    fighter2: null,
+    fighter1El: null,
+    fighter2El: null,
+
+    start(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;       // Personagem 1
+        this.fighter2 = fighter2;       // Personagem 2
+        this.fighter1El = fighter1El;   // Elemento 1 ação
+        this.fighter2El = fighter2El;   // Elemento 2 ação
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+
+        this.update();
+    },
+    update() {
+        // Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`; // Coloca o nome e o HP
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;  // Calculo para definir a vida
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`; // Estilizando meu css com o valor do calculo
+
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    },
+    doAttack(attacking, attacked) {
+        if (attacking.life <= 0 || attacked.life <= 0) {
+            log.addMessage('Alguém tá morto, não pode atacar.');
+            return;
+        }
+
+        const attackFactor = (Math.random() * 2).toFixed(2);
+        const defenseFactor = (Math.random() * 2).toFixed(2);
+
+        const actualAttack = attacking.attack * attackFactor;
+        const actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            attacked.life = attacked.life < 0 ? 0 : attacked.life;
+            log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`);
+        } else {
+            log.addMessage(`${attacked.name} consegiu defender...`);
+        }
+
+
+        this.update();
+    }
+}
+const log = {
+    list: [],
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+    },
+    render() {
+        const logEl = document.querySelector('.log');
+        logEl.innerHTML = '';
+        for (let i = 0; i < this.list.length; i++) {
+            logEl.innerHTML += `<li>${this.list[i]}</li>`;
+        }
+    }
+};
+
---
+++ Aulas-Boniek/LutaFuncional/assets/js/script.js
@@ -0,0 +1,24 @@
+// Criando personagem -  inicio
+const char = createKnight('Emerson');
+const monster = createLittleMonster();
+// Criando personagem -  inicio
+
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
